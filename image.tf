081fa122d3db8d667fdc29f9f2c2e104b1a25d4e
Angel Nunez Mencias <anunez@de.ibm.com>
2022-07-08 00:28:34 +0200

    refresh image when qcow2 changes

--- image.tf
+++ image.tf
@@ -1,4 +1,18 @@
+# The value of the qcow2 is depending on the bucket object etag
+resource "null_resource" "value_of_qcow2" {
+  depends_on = [
+    data.ibm_cos_bucket_object.image_qcow2
+  ]
+
+  triggers = {
+    etag = data.ibm_cos_bucket_object.image_qcow2.etag
+  }
+}
+
 resource "ibm_is_image" "custom_image" {
+  depends_on = [
+    null_resource.value_of_qcow2
+  ]
   name               = var.custom_image_name
   href               = data.ibm_cos_bucket_object.image_qcow2.object_sql_url
   operating_system   = var.custom_image_os
@@ -7,5 +21,8 @@ resource "ibm_is_image" "custom_image" {
 
   timeouts {
     create = "300m"
+  }
+  lifecycle {
+    replace_triggered_by = [null_resource.value_of_qcow2.id]
   }
 }
