0fd0037fdc30db43fcafb6fc6402b7cbca8e82ed
Angel Nunez Mencias <anunez@de.ibm.com>
2022-07-07 09:59:31 +0200

    increase timeouts

--- image.tf
+++ image.tf
@@ -3,5 +3,9 @@ resource "ibm_is_image" "custom_image" {
   href               = data.ibm_cos_bucket_object.image_qcow2.object_sql_url
   operating_system   = var.custom_image_os
   #encrypted_data_key = "eJxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx0="
-  #encryption_key     = "crn:v1:bluemix:public:kms:us-south:a/6xxxxxxxxxxxxxxx:xxxxxxx-xxxx-xxxx-xxxxxxx:key:dxxxxxx-fxxx-4xxx-9xxx-7xxxxxxxx"
+  #encryption_key     = "crn:v1:bluemix:public:kms:us-south:a/6xxxxxxxxxxxxxxx:xxxxxxx-xxxx-xxxx-xxxxxxx:key:dxxxxxx-fxxx-4xxx-9xxx-7xxxxxxxx"//increase timeouts as per volume size
+
+  timeouts {
+    create = "90m"
+  }
 }
--- snapshot.tf
+++ snapshot.tf
@@ -1,4 +1,10 @@
 resource "ibm_is_snapshot" "custom_image_data_volume" {
   name          = "${var.volume_prefix}-data"
   source_volume = ibm_is_instance_volume_attachment.zos_volumes["data"].volume
+
+  //increase timeouts as per volume size
+  timeouts {
+    create = "45m"
+  }
+  
 }
