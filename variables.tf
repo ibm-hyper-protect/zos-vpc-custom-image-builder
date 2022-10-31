377686552bda89c1aba3554ba5e296fd57e59df0
vishwabmc <vishwanath@in.ibm.com>
2022-10-31 13:55:47 +0530

    Update variables and descriptions
    Signed-off-by: vishwabmc <vishwanath@in.ibm.com>

--- variables.tf
+++ variables.tf
@@ -1,53 +1,56 @@
 # Image variables
 variable "custom_image_name" {
-  description = "z/OS Custom image name" 
   type        = string
-  default = "wazi-custom-image"
+  default     = "wazi-custom-image"
+  description = "Name of the z/OS custom image that will be generated"
 }
 
 variable "custom_vsi"{
+  type        = bool
+  default     = false
   description = "Builds VSI using the custom image"
-  type = bool
-  default = true
 }
 variable "custom_image_os" {
+  default     = "zos-2-4-s390x-dev-test-byol"
   description = "OS for generated image - it must match the OS uploaded to COS"
-  default = "zos-2-4-s390x-dev-test-byol"
 }
 
 # Target location for image
 variable "region" {
-  description = "Region to run the VSI doing the conversion" 
   type        = string
-  default = "ca-tor"
+  default     = "ca-tor"
+  description = "Region to run the VSI doing the conversion. Custom image will be stored here"
 }
 
 variable "zone" {
-  description = "Zone for the VSI - data volumes will be stored here" 
   type        = string
-  default = "1"
+  default     = "1"
+  description = "Zone for the VSI - data volumes will be stored here"
 }
 
-# Souce COS bucket with the CKD files
+# Source COS bucket with the CKD files
 variable "cos_bucket_region" {
-  description = "Region of the COS instance" 
   type        = string
+  description = "Region of the COS bucket"
 }
 
 variable "cos_instance_name" {
-  description = "Name of the COS instance" 
   type        = string
+  description = "Name of the COS instance that holds the COS bucket"
 }
 
 variable "cos_bucket_name" {
-  description = "Name of the COS bucket" 
   type        = string
+  description = <<-DESC
+                 Name of the COS bucket that contains the z/OS volumes archive 
+                 files uploaded by on-prem Wazi Image Builder
+                DESC
 }
 
 variable "cos_resource_group" {
-  description = "Resource group of the COS instance"
   type        = string
   default     = "Default"
+  description = "Resource group of the COS instance"
 }
 
 variable "cos_endpoint" {
@@ -106,8 +109,8 @@ variable "volume_purpose" {
 }
 
 variable "cos_bucket_type" {
+  default     = "region_location"
   description = "bucket type"
-  default = "region_location"
 }
 
 # ssh private key to establish a session with data mover VSI
@@ -142,25 +145,25 @@ variable "ssh_public_key" {
 }
 
 variable "total_ipv4_address_count" {
+    default     = 256
     description = "total IPs for subnetwork"
-    default = 256
 }
 
 variable "mover_image_name" {
   # Regular expresions allowed
   # Only Terraform 1.2 can connect to Ubuntu 22.
   # Ubuntu 20.04 is more widely supported
-  description = "image used for the VSI data mover"
   default = ".*ubuntu.*20-04.*amd64.*"
+  description = "image used for the VSI data mover"
 }
 
 variable "mover_profile" {
+  default     = "bx2d-16x64"
   description = "image used for the VSI data mover"
-  default = "bx2d-16x64"
 }
 
 variable "iam_endpoint_url" {
-    description = "IAM endpoint url"
     type        = string
     default     = "https://iam.cloud.ibm.com"
+    description = "IAM endpoint url"
 }
